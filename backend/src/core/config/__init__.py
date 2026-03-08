99c2e1fc1a50e6bd5e2511ba4f9fdb2ebe66de6a
Apolo <polonnikov.alexander@gmail.com>
2026-03-08 20:03:32 +0300

    feat(backend): add LoggingSettings class for logging configuration

---
+++ backend/src/core/config/__init__.py
@@ -0,0 +1,3 @@
+from .classes import LoggingSettings
+
+__all__ = ("LoggingSettings",)
--- backend/src/core/config/classes.py
+++ backend/src/core/config/classes.py
@@ -1,5 +1,6 @@
 from typing import Literal
 
+from pydantic import BaseModel, HttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 type LogLevel = Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
@@ -17,3 +18,15 @@ class BaseConfiguration(BaseSettings):
         frozen=True,
         extra="ignore",
     )
+
+
+class LoggingSettings(BaseModel):
+    """Logging settings configuration."""
+
+    log_level: LogLevel = "DEBUG"
+    log_format: str = "%(asctime)s %(levelname)6s %(name)s: %(message)s"
+    log_date_format: str = "%Y-%m-%d %H:%M:%S"
+    sentry_dsn: HttpUrl | None = None
+    sentry_traces_sample_rate: float = 1.0
+    sentry_log_level: LogLevel = "ERROR"
+
