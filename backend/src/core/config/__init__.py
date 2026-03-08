d0f148b0fd82ae6105a3b30cb2e00711d59004fb
Apolo <polonnikov.alexander@gmail.com>
2026-03-08 21:14:37 +0300

    feat(config): export APP_SETTINGS and setup_logging

--- backend/src/core/config/__init__.py
+++ backend/src/core/config/__init__.py
@@ -1,8 +1,12 @@
 from .classes import ApiPrefix, DatabaseSettings, LoggingSettings, ProjectSettings
+from .logging import setup_logging
+from .settings import APP_SETTINGS
 
 __all__ = (
+    "APP_SETTINGS",
     "ApiPrefix",
     "DatabaseSettings",
     "LoggingSettings",
     "ProjectSettings",
+    "setup_logging",
 )
