288238221f02076a2548a5a1b2c2885bd5a71a6c
Apolo <polonnikov.alexander@gmail.com>
2026-03-09 21:10:31 +0300

    feat(backend): add uvicorn dev server setup with logging

--- backend/run_dev.py
+++ backend/run_dev.py
@@ -1,5 +1,19 @@
+import uvicorn
+
+from src.core.config import APP_SETTINGS
+from src.core.config.logging import setup_logging
+
+
 def main() -> None:
     """Main function."""
+    setup_logging()
+
+    uvicorn.run(
+        "src.main:app",
+        host=APP_SETTINGS.UVICORN.host,
+        port=APP_SETTINGS.UVICORN.port,
+        reload=APP_SETTINGS.UVICORN.reload,
+    )
 
 
 if __name__ == "__main__":
