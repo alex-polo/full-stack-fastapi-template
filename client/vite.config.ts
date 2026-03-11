c930c9118981a3cc47cfee355eeb1f3b73dfe115
Apolo <polonnikov.alexander@gmail.com>
2026-03-11 14:04:11 +0300

    feat(client): add dynamic proxy config via env vars for dev

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,26 @@
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
 
-  // server: {
-  //   proxy: {
-  //     '/api': 'http://localhost:8085',
-  //   },
-  //   host: '0.0.0.0',
-  //   port: 5173,
-  // },
+  return {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    plugins: [react()],
+
+    server: {
+      proxy: {
+        [env.VITE_API_URL || '/api']:
+          `http://${env.VITE_BACKEND_HOST || 'localhost'}:${env.VITE_BACKEND_PORT || '8000'}`,
+      },
+      host: '0.0.0.0',
+      port: 5173,
+    },
+  };
 });
