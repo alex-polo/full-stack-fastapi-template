1e76c21bd408c0a4d083b5e002310986a08e8465
Apolo <polonnikov.alexander@gmail.com>
2026-03-13 06:11:34 +0300

    refactor(db): remove unused SQLAUnitOfWork export

--- backend/src/core/database/__init__.py
+++ backend/src/core/database/__init__.py
@@ -1,10 +1,8 @@
 from .base import Base
 from .engine import DB_HANDLER, DBSessionDep
-from .unitofwork import SQLAUnitOfWork
 
 __all__ = (
     "DB_HANDLER",
     "Base",
     "DBSessionDep",
-    "SQLAUnitOfWork",
 )
