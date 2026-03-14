26cbfb8012858bb6cddfa827fff9d7b67c111705
Apolo <polonnikov.alexander@gmail.com>
2026-03-14 21:48:52 +0300

    test(health-check): convert health check test to async

--- backend/tests/api/test_health_check.py
+++ backend/tests/api/test_health_check.py
@@ -5,13 +5,12 @@ from typing import TYPE_CHECKING
 from fastapi import status
 
 if TYPE_CHECKING:
-    from fastapi.testclient import TestClient
-    from httpx import Response
+    from httpx import AsyncClient, Response
 
 
-def test_health_check(client: TestClient) -> None:
+async def test_health_check(client: AsyncClient) -> None:
     """Test health check endpoint."""
-    response: Response = client.get("/api/v1/utils/health-check")
+    response: Response = await client.get("/api/v1/utils/health-check")
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"status": "healthy"}
