2f38e3e669a055610256b8986a337260f8b3d03f
Apolo <polonnikov.alexander@gmail.com>
2026-03-15 16:58:10 +0300

    feat(client): add main App component with routing and theming

---
+++ client/src/app/index.tsx
@@ -0,0 +1,19 @@
+import { theme } from '@/shared/config';
+import { BreadcrumbsProvider, router } from '@/shared/lib';
+import { GlobalLoader } from '@/shared/ui/loaders';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Suspense } from 'react';
+import { RouterProvider } from 'react-router-dom';
+
+export const App = () => {
+  return (
+    <BreadcrumbsProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Suspense fallback={<GlobalLoader />}>
+          <RouterProvider router={router} />
+        </Suspense>
+      </ThemeProvider>
+    </BreadcrumbsProvider>
+  );
+};
