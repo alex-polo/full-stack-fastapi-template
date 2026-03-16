90da388ba62c04df0f083a47b4e8481546c7dfd8
Apolo <polonnikov.alexander@gmail.com>
2026-03-16 20:46:45 +0300

    feat(client): add PublicHeader and Footer to BaseLayout

--- client/src/widgets/base-layout/ui/BaseLayout.tsx
+++ client/src/widgets/base-layout/ui/BaseLayout.tsx
@@ -1,14 +1,16 @@
-import { Breadcrumbs } from '@/widgets/breadcrumbs';
+import { Breadcrumbs, Footer, PublicHeader } from '@/widgets';
 import { Box, Container } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
 export const BaseLayout = () => {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <PublicHeader />
       <Container component="main" sx={{ flexGrow: 1, py: 3 }}>
         <Breadcrumbs />
         <Outlet />
       </Container>
+      <Footer />
     </Box>
   );
 };
